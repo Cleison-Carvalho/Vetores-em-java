24af4460ecba82e4830325bc3bb49ea7ea0cbb59
Cleison Carvalho <ads.uninassau.patteo@gmail.com>
2025-03-06 12:40:09 -0300

    Inputando elementos dentro de um array e pecorrendo-os

---
+++ Vetor.java
@@ -0,0 +1,30 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Vetor {
+    
+    public static void main(String[] args) {
+
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.println("***INPUTANDO ELEMENTO EM UM VETOR E PECORRENDO A ESTES ELEMENTOS*** ");
+        System.out.print("QUANTOS NUMEROS VOCE VAI DIGITAR ? ");
+        int N = sc.nextInt();
+        
+        double[] vet = new double[N];
+        
+        for (int i = 0; i < N; i++) {
+            System.out.print("Digite um numero: ");
+            vet[i] = sc.nextDouble();
+        }
+
+        System.out.println("NUMEROS DIGITADOS: ");
+        for (int i = 0; i < N; i++) {
+            System.out.println(String.format("%.1f", vet[i]));
+        }
+        
+        sc.close();
+
+    }
+}
